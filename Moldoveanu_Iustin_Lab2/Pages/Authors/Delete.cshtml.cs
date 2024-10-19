9425d46f81b89c0d5ff386e73e3b669cd0227f29
Moldoveanu Iustin-Eusebiu <147540004+moldoveanu-iustin@users.noreply.github.com>
2024-10-19 16:12:44 +0300

    Add files via upload

---
+++ Moldoveanu_Iustin_Lab2/Pages/Authors/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Moldoveanu_Iustin_Lab2.Data;
+using Moldoveanu_Iustin_Lab2.Models;
+
+namespace Moldoveanu_Iustin_Lab2.Pages.Authors
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Moldoveanu_Iustin_Lab2.Data.Moldoveanu_Iustin_Lab2Context _context;
+
+        public DeleteModel(Moldoveanu_Iustin_Lab2.Data.Moldoveanu_Iustin_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Author Authors { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var authors = await _context.Authors.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (authors == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Authors = authors;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var authors = await _context.Authors.FindAsync(id);
+            if (authors != null)
+            {
+                Authors = authors;
+                _context.Authors.Remove(Authors);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Moldoveanu_Iustin_Lab2/Pages/Authors/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Moldoveanu_Iustin_Lab2.Data;
+using Moldoveanu_Iustin_Lab2.Models;
+
+namespace Moldoveanu_Iustin_Lab2.Pages.Authors
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Moldoveanu_Iustin_Lab2.Data.Moldoveanu_Iustin_Lab2Context _context;
+
+        public DetailsModel(Moldoveanu_Iustin_Lab2.Data.Moldoveanu_Iustin_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Author Authors { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var authors = await _context.Authors.FirstOrDefaultAsync(m => m.Id == id);
+            if (authors == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Authors = authors;
+            }
+            return Page();
+        }
+    }
+}
